5fa5afab78418f8360dae7688651589779d7d91a
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-24 20:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let level = document.querySelectorAll("ul>li")
+level.addEventListner("click",()=>{
+	for(let i=0;i<level.length;i++) {
+		if(level[i].id=="level"){
+			alert('the level of the element is: '+(i+1))
+		}
+	}
+})
